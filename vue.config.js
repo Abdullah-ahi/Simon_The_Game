697a5f4136afeb079a23c752700a4212eb5fe870
abby <www.sar.ras@gmail.com>
2021-03-26 00:28:50 +0300

    updated project

--- vue.config.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  publicPath: /Simon_The_Game/
-}
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const dotenv = require("dotenv-webpack");
 
 module.exports = {
   entry: path.resolve(__dirname, 'src', 'index'),
@@ -48,6 +47,5 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: 'styles.css'
     }),
-    new dotenv(),
   ]
 }
